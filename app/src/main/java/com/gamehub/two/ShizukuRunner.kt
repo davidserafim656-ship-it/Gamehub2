ad5c89b47d0145aff7f6393003b5c3990851c8e5
deathlanger66 <josy17813@gmail.com>
2026-05-16 03:31:34 -0300

    Atualizar o ShizukuRunner.kt

--- app/src/main/java/com/gamehub/two/ShizukuRunner.kt
+++ app/src/main/java/com/gamehub/two/ShizukuRunner.kt
@@ -1,11 +1,14 @@
 package com.gamehub.two
 
-import rikka.shizuku.Shizuku
-
 object ShizukuRunner {
+
     fun run(cmd: String) {
         try {
-            Shizuku.newProcess(arrayOf("sh", "-c", cmd), null, null)
-        } catch (_: Exception) {}
+            Runtime.getRuntime().exec(
+                arrayOf("sh", "-c", cmd)
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
